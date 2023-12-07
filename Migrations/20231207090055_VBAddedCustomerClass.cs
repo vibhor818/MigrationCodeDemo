6564bd347aa3152e5d71589093df09204cdfbc2b
vibhoraz400 <vibhoraz400@outlook.com>
2023-12-07 15:35:25 +0530

    Add project files.

---
+++ Migrations/20231207090055_VBAddedCustomerClass.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrationCodeDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class VBAddedCustomerClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ MyContexts/VBContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationCodeDemo.MyContexts
+{
+    public class VBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data source=(localdb)\mssqllocaldb;database=migrationbtest;trusted_connection=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? CustomerAddress { get; set; }
+        public string? CustomerMobile { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+    }
+    public class Order
+    {
+        public int OrderId { get; set;}
+        public string OrderName { get; set; }
+        public string OrderDesc { get; set; }
+        public Customer Customer { get; set; }
+    }
+    
+}
